607e4b0f54d2b39bbb8099b010efd23e6654ff18
PravalKumarWipro <praval.kumar@wipro.com>
2024-02-24 09:24:59 +0000

    Swagger completed

--- src/main/java/com/cacheing/cacheingtest/CacheingtestApplication.java
+++ src/main/java/com/cacheing/cacheingtest/CacheingtestApplication.java
@@ -2,8 +2,12 @@ package com.cacheing.cacheingtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2; 
+
+
 
 @SpringBootApplication
+@EnableSwagger2
 public class CacheingtestApplication {
 
 	public static void main(String[] args) {
